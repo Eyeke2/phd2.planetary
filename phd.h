b58ea964fd8b6ca0fff79e4c331dbb05db16e89c
Leo Shatz <leonid.shatz@gmail.com>
2023-12-10 18:01:45 +0200

    bump subversion to planet.4
    Signed-off-by: Leo Shatz <leonid.shatz@gmail.com>

--- phd.h
+++ phd.h
@@ -79,7 +79,7 @@
 
 #define APPNAME _T("PHD2 Guiding")
 #define PHDVERSION _T("2.6.12")
-#define PHDSUBVER _T("planet.3")
+#define PHDSUBVER _T("planet.4")
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
