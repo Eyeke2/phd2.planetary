133ccca858ba130da52f00870eb4b93598d5201e
Leo Shatz <leonid.shatz@gmail.com>
2023-12-27 15:40:53 +0200

    Planetary tool: minor code refactoring.
    Signed-off-by: Leo Shatz <leonid.shatz@gmail.com>

--- eegg.cpp
+++ eegg.cpp
@@ -40,7 +40,6 @@
 #include "calreview_dialog.h"
 #include "nudge_lock.h"
 #include "comet_tool.h"
-#include "planetary_tool.h"
 #include "guiding_assistant.h"
 #include "calibration_assistant.h"
 
@@ -266,19 +265,6 @@ void MyFrame::OnCometTool(wxCommandEvent& WXUNUSED(evt))
     if (pCometTool)
     {
         pCometTool->Show();
-    }
-}
-
-void MyFrame::OnPlanetTool(wxCommandEvent& WXUNUSED(evt))
-{
-    if (!pPlanetTool)
-    {
-        pPlanetTool = PlanetTool::CreatePlanetToolWindow();
-    }
-
-    if (pPlanetTool)
-    {
-        pPlanetTool->Show();
     }
 }
 
