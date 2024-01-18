996de68fa76be163ae5d68d5fcebd41fbef56d8d
Leo Shatz <leonid.shatz@gmail.com>
2024-01-18 18:25:10 +0200

    Planetary tool: bump rev to v2.6.13-planet.dev4.rc2
    Signed-off-by: Leo Shatz <leonid.shatz@gmail.com>

--- phd.h
+++ phd.h
@@ -87,7 +87,7 @@
 
 #define APPNAME _T("PHD2 Guiding")
 #define PHDVERSION _T("2.6.13")
-#define PHDSUBVER _T("-planet.dev4.rc1" PHDEXSUFFIX)
+#define PHDSUBVER _T("-planet.dev4.rc2" PHDEXSUFFIX)
 #define FULLVER PHDVERSION PHDSUBVER
 
 #if defined (__WINDOWS__)
