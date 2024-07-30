7e77cea965dda47a7c149d6590057a16f4177303
Leo Shatz <leonid.shatz@gmail.com>
2024-07-30 19:30:01 +0300

    Changed version labeling to catch up with PHD2 master branch: planetary version is based on development label followed by '-planet' suffix with a subversion number. This version will be therefore v2.6.15dev5-planet.rc1.
    Signed-off-by: Leo Shatz <leonid.shatz@gmail.com>

--- phd.h
+++ phd.h
@@ -81,7 +81,7 @@
 
 #define APPNAME _T("PHD2 Guiding")
 #define PHDVERSION _T("2.6.13")
-#define PHDSUBVER _T("-planet.dev7")
+#define PHDSUBVER _T("dev5-planet.rc1")
 #ifdef DEVELOPER_MODE
 #define FULLVER PHDVERSION PHDSUBVER _T(" (Developer mode)")
 #else
