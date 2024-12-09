7cee53056ba177587c67f58e8982f3e2207ed708
Leo Shatz <leonid.shatz@gmail.com>
2024-12-09 21:45:22 +0200

    Added notification to the planetary module about the capture state.
    Signed-off-by: Leo Shatz <leonid.shatz@gmail.com>

--- src/myframe.cpp
+++ src/myframe.cpp
@@ -1214,6 +1214,9 @@ void MyFrame::UpdateButtonsStatus()
     if (pierFlipToolWin)
         PierFlipTool::UpdateUIControls();
 
+    if (pGuider->m_SolarSystemObject.UpdateCaptureState(CaptureActive))
+        need_update = true;
+
     if (need_update)
     {
         if (pGuider->GetState() < STATE_SELECTED)
