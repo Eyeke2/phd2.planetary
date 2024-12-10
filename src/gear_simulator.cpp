705ff1ca4e2cd5530703a94c85ea997ac3c2e6f6
Leo Shatz <leonid.shatz@gmail.com>
2024-12-10 11:57:44 +0200

    Closed the distance error measurement loop in camera simulation mode, used for planetary detection statistics.
    Signed-off-by: Leo Shatz <leonid.shatz@gmail.com>

--- src/gear_simulator.cpp
+++ src/gear_simulator.cpp
@@ -1602,6 +1602,9 @@ bool CameraSimulator::Capture(int duration, usImage& img, int options, const wxR
         double rx, ry;
         sim.SimulateDisplacement(rx, ry);
 
+        // Save actual simulator displacement for tracking accuracy error analysis
+        pFrame->pGuider->m_SolarSystemObject.SaveCameraSimulationMove(rx, ry);
+
         // Translate the image by shifting it few pixels
         double borderValue = calculateBorderAverage(*disk_image);
         cv::Mat translatedImage;
@@ -1623,6 +1626,8 @@ bool CameraSimulator::Capture(int duration, usImage& img, int options, const wxR
         // Finally, render clouds
         if (SimCamParams::clouds_opacity > 0)
         {
+            if (pFrame->pGuider->m_SolarSystemObject.Get_SolarSystemObjMode())
+                subframe = wxRect(0, 0, FullSize.x, FullSize.y);
             render_clouds(img, subframe, duration, 30, 100);
         }
         Debug.Write(wxString::Format("Simulator: loaded image file: %s\n", filename));
