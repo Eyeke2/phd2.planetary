63f21355ebd347072ba7f65d4ae0721587f7bc7a
Andy Galasso <andy.galasso@gmail.com>
2025-12-31 17:06:46 -0500

    code cleanup: use wxClip (#1395)
    Minor code cleanup: use wxClip in place of wxMin + wxMax.

--- src/gear_simulator.cpp
+++ src/gear_simulator.cpp
@@ -176,7 +176,7 @@ wxString SimCamParams::SimFileTemplate = _("C:\\Temp\\phd2\\sim_image.png");
 // Needed to handle legacy registry values that may no longer be in correct units or range
 static double range_check(double thisval, double minval, double maxval)
 {
-    return wxMin(wxMax(thisval, minval), maxval);
+    return wxClip(thisval, minval, maxval);
 }
 
 static void load_sim_params()
